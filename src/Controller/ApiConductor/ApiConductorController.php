47cf5d9286221d380e1262b7a8e8ff1cd44e3483
Mario A. Estrada Zuluaga <maestradaz3@gmail.com>
2018-10-02 18:56:51 -0500

    Fos rest full

--- src/Controller/ApiConductor/ApiConductorController.php
+++ src/Controller/ApiConductor/ApiConductorController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\ApiConductor;
 
 
+use App\Entity\Operador;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,30 +11,33 @@ use Symfony\Component\HttpFoundation\Request;
 class ApiConductorController extends FOSRestController
 {
     /**
-     * @Rest\Get("/api/conductor/despacho/guias/{codigoDespacho}", name="api_conductor_despacho_guias")
+     * @Rest\Get("/api/conductor/despacho/guias/{codigoOperador}/{codigoDespacho}", name="api_conductor_despacho_guias")
      */
-    public function guia(Request $request, $codigoDespacho)
+    public function guia(Request $request, $codigoOperador, $codigoDespacho)
     {
+
         set_time_limit(0);
         ini_set("memory_limit", -1);
-        /*$em = $this->getDoctrine()->getManager();
-        $query = $em->createQuery(
-            'SELECT g.codigoGuiaPk, 
-        WHERE g.codigoDespachoFk = :codigoDespacho'
-        )->setParameter('codigoDespacho', 1);
+        $em = $this->getDoctrine()->getManager();
+        $arOperador =$em->getRepository(Operador::class)->find($codigoOperador);
+        $direccion = $arOperador->getUrlServicio();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $direccion . "/transporte/api/guia/despacho/$codigoDespacho");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json')); // Assuming you're requesting JSON
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $response = curl_exec($ch);
+        $arrGuias = json_decode($response, true);
+        if($arrGuias['error']) {
+            return null;
+        } else {
+            return $arrGuias['guias'];
+        }
+        //return $arrGuias;
 
-        $qb = $em->createQueryBuilder();
-        $qb->from(TteGuia::class, "g")
-            ->select("g.codigoGuiaPk")
-            ->where("g.codigoDespachoFk = 1");
-        return $qb->getQuery()->getResult();*/
-
-        //return $query->execute();
-
-        $prueba = array();
+        /*$prueba = array();
         $prueba[] = array('codigoGuiaPk' => 11111);
         $prueba[] = array('codigoGuiaPk' => 22222);
-        return $prueba;
+        return $prueba;*/
     }
 
 
